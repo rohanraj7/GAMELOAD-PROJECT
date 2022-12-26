20f8c71c27b61ece1bff6e9015e4b0014ddfff73
rohanraj7 <rohanraj9645@gmail.com>
2022-12-26 16:36:38 +0530

    edited

--- gameuser/views.py
+++ gameuser/views.py
@@ -160,10 +160,10 @@ def signup(request):
         else:
             request.session['phoneno']=phoneno
             phone=phoneno
-            # verify.send(phone)
+            verify.send(phone)
             user = User.objects.create_user( fullname=fullname,phoneno=phoneno ,email=email, password=password1)
             user.save()
-            return redirect(login_view)
+            return redirect(otp)
     return render(request, 'signup.html')
 
 
